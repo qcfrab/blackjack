21685d4aeed31ea8c824ce0cf7237adcbba81642
Janna <ostrovskaazanna8@gmail.com>
2026-03-14 22:44:29 +0300

    Add Card class

---
+++ lib/blackjack/card.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+module Blackjack
+  class Card
+    attr_reader :rank, :suit
+
+    def initialize(rank, suit)
+      @rank = rank
+      @suit = suit
+    end
+
+    def to_s
+      "#{rank}#{suit}"
+    end
+
+    def value
+      case rank
+        when 'J', 'Q', 'K' then 10
+        when 'A'           then 11
+        else rank.to_i
+      end
+    end
+  end
+end
