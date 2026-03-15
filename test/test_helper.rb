88e2b74b7c3fb04967afb3b6d5a68d2a9954acae
Janna <ostrovskaazanna8@gmail.com>
2026-03-15 20:44:35 +0300

    Setup auto-run test helper

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,3 +4,7 @@ $LOAD_PATH.unshift File.expand_path("../lib", __dir__)
 require "blackjack"
 
 require "minitest/autorun"
+
+Dir[File.expand_path("*_test.rb", __dir__)].each do |file|
+  require file
+end
